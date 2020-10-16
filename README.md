6c0bf9f90c08e61329d5b2410914d596f2dd8427
jankiel7410 <michal7410@gmail.com>
2020-10-16 20:27:56 +0200

    add first test

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
 
 * załóż konto na github
 * sklonuj to repozytorium: `git clone https://github.com/jankiel7410/zadanka.git`
-* stwórz branch "podstawy": `git checkout -b podstawy`
+* zainstaluj zależności projektu: `npm i`
+* stwórz branch "fundamentals": `git checkout -b fundamentals`
 * rozwiąż zadania.
 * utwórz pull request. Jeśli nie wiesz jak, to ci pomogę.
+
+
+
+# Uruchamianie testów
+Każde zadanie to osobny plik. Żeby sprawdzić czy rozwiązanie jest prawidłowe, uruchom komendę  `npx jest <file>`, np. `npx jest fundamentals/first_steps.test.js`
+
+Protip: możesz dodać flagę `--watch` żeby dostawać feedback z testów za każdym razem kiedy zmieniasz plik.
---
+++ fundamentals/first_steps.test.js
@@ -0,0 +1,41 @@
+
+/**
+ * fizz-buzz. Napisz funkcję, która dla zadanego n zwróci:
+ * jeśli n jest podzielne przez 3: "fizz" 
+ * jeśli n jest podzielne przez 5: "buzz" 
+ * jeśli n jest podzielne przez 15: "fizzbuzz" 
+ * jeśli n nie jest podzielne ani przez 3, ani przez 5: n (czyli po prostu liczbę która jest na wejściu)
+ */
+
+ function fizzbuzz(n) {
+     // your code here
+ }
+
+
+
+ it('returns fizz for multiples of 3', () => {
+    expect(fizzbuzz(3)).toEqual('fizz')
+    expect(fizzbuzz(6)).toEqual('fizz')
+    expect(fizzbuzz(33)).toEqual('fizz')
+})
+
+it('returns buzz for multiples of 5', () => {
+    expect(fizzbuzz(5)).toEqual('buzz')
+    expect(fizzbuzz(10)).toEqual('buzz')
+    expect(fizzbuzz(500)).toEqual('buzz')
+})
+
+
+it('returns fizzbuzz for multiples of 3 and 5', () => {
+    expect(fizzbuzz(15)).toEqual('fizzbuzz')
+    expect(fizzbuzz(30)).toEqual('fizzbuzz')
+    expect(fizzbuzz(300)).toEqual('fizzbuzz')
+})
+
+
+it("returns a number for input that's not a multiple of 3 or 5", () => {
+    expect(fizzbuzz(1)).toEqual(1)
+    expect(fizzbuzz(4)).toEqual(4)
+    expect(fizzbuzz(17)).toEqual(17)
+})
+

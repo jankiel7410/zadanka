7a49ef89b52b9a73ed1eb4f6ef91199632e788f0
pablo7410 <pablo23k8@gmail.com>
2020-11-04 20:26:05 +0100

    updated 2.test.js

--- arrays/2.test.js
+++ arrays/2.test.js
@@ -12,7 +12,16 @@
 
 
 function bubbleSort(arr, comparator) {
-    return arr
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = 0; j < arr.length; j++) {
+            if (comparator(arr[j], arr[j+1]) == 1) {
+                let tmp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = tmp;
+            }
+        }
+    }
+    return arr;
 }
 
 describe('bubbleSort', () => {
